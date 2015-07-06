6a3c389022741a59827a6ec2e636e5dd203a89cb
Neerajen Sritharan <neerajensritharan@C7-DeepBlue.(none)>
2015-07-06 13:29:22 -0400

    squaring works!

---
+++ matlabDriverScript.m
@@ -0,0 +1,16 @@
+function [transformed] = matlabDriverScript(x)
+
+%square every element in array
+gpuDevice(1); 
+
+k = parallel.gpu.CUDAKernel('square.ptx','square.cu');
+
+%set object properties 
+k.GridSize = [3 1 1];
+k.ThreadBlockSize = [3 1 1];
+
+result = feval(k,x);
+transformed = gather(result); 
+
+
+end
